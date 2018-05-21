642f86cce7e539499c42a5dcf17b532f763b3665
wdbrow11 <will.brown.hc@gmail.com>
2018-05-21 00:15:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-scuberGreetingForFeet(numFeet) {
+scuberGreetingForFeet() {
   let numFeet = 3000
   if ( numFeet > 2500 ) {
     document.write( "We do not allow rides over 2500 feet" )
   }
   else if ( numFeet > 2000 ) {
-    document.write( "That costs an extra $30.")
+    document.write( "That costs an extra $30." )
   }
   else if ( numFeet <= 400 ) {
-    document.write( "Enjoy your free sample!")
+    document.write( "Enjoy your free sample!" )
   }  
 }
